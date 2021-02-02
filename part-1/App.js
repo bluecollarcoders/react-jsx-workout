1ea5479f8b17bb87542f9409e68d7e46f5ee88c5
Caleb Matteis <matteismusic@gmail.com>
2021-02-02 06:25:42 -0500

    first commit

---
+++ part-1/App.js
@@ -0,0 +1,20 @@
+const App = () => {
+    return (
+        <div>
+            <FirstComponent />
+            <NamedComponent name="Caleb" />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"))
